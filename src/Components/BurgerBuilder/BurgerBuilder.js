70fad570a5128d0a2587260c53fc28e0657eb003
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-12-29 21:27:53 +0600

    burger increasing working

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import Controls from '../Controls/Controls';
 import Burger from './Burger/Burger';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: [
+      { type: 'bread-salad', amount: 1 },
+      { type: 'bread-cheese', amount: 2 },
+      { type: 'bread-meat', amount: 3 }
+    ]
+  }
+
+  addIngredient = type => {
+    console.log(type);
+    const ingredients = [...this.state.ingredients];
+    for (let item of ingredients) {
+      if (item.type === type) {
+        item.amount++
+      }
+    }
+    this.setState({ ingredients: ingredients })
+  }
+
+  removeIngredient = type => {
+    console.log(type);
+  }
+
   render() {
     return (
-      <div>
-        <Burger></Burger>
+      <div className='d-flex flex-md-row flex-column'>
+        <Burger ingredients={this.state.ingredients}></Burger>
+        <Controls
+          ingredientAdded={this.addIngredient}
+          ingredientRemove={this.removeIngredient}
+        ></Controls>
       </div>
     )
   }
